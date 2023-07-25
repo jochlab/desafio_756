56967cd73d1d33deb79a4551304779b5ba9fb482
Xosh <xoshdev@gmail.com>
2023-07-25 16:24:16 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,3 +3,22 @@ var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
     return new bootstrap.Tooltip(tooltipTriggerEl)
 })
 
+$("#torrespaine").click(function () {
+    alert("Quedan solamente 5 paquetes ¡reserva ahora!");
+});
+
+$("#atacama").click(function () {
+    alert("Quedan solamente 2 paquetes ¡reserva ahora!");
+});
+
+$("#norte").click(function () {
+    alert("8 paquetes comprados hace 15 minutos ¡reserva ahora!");
+});
+
+$("#araucania").click(function () {
+    alert("¡reserva este Paquete y estarás apoyando a las Comunidades locales!");
+});
+
+$("#contactenos").click(function () {
+    alert("Su mensaje ha sido enviado exitosamente.");
+});
